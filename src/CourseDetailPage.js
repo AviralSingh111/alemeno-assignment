ee6bdb1cf3e2a9e62b747f3ac08a2fe8a4a3fd79
Aviral Singh <aviralsingh360@gmail.com>
2024-08-28 23:53:26 +0530

    second commit

--- src/CourseDetailPage.js
+++ src/CourseDetailPage.js
@@ -37,7 +37,7 @@ const CourseDetailPage = () => {
           {course.enrollmentStatus === 'Open' && (
             <button
               onClick={handleEnroll}
-              className="mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              className="mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-5"
             >
               Enroll Now
             </button>
